3433c985116728f681762aff777bf7981b5f9bb1
Vansyptra <108278556+Vansyptra@users.noreply.github.com>
2022-08-28 17:34:34 +0700

    Initial commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+        
+        <meta charset="utf-8" />
+        <title>Login Dhapu Music</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesdesign" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/dp.png">
+
+        <!-- Bootstrap Css -->
+        <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body class="bg-pattern">
+        <div class="bg-overlay"></div>
+        <div class="account-pages my-5 pt-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-4 col-lg-6 col-md-8">
+                        <div class="card">
+                            <div class="card-body p-4">
+                                <div class="">
+                                    <div class="text-center">
+                                        <a href="index.php" class="">
+                                            <img src="assets/images/dp.png" alt="" height="50" class="auth-logo logo-dark mx-auto">
+                                        </a>
+                                    </div>
+                                    <!-- end row -->
+                                    <h4 class="font-size-18 text-muted mt-2 text-center">Selamat Datang</h4><p></p>
+                                    <form method="post" class="form-horizontal" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="mb-4">
+                                                    <label class="form-label" for="username">{{ __('Username') }}</label>
+                                                    <input type="text" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required autocomplete="email" placeholder="Enter username" name="email">
+                                                    @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                                </div>
+                                                <div class="mb-4">
+                                                    <label class="form-label" for="userpassword">{{ __('Password') }}</label>
+                                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Enter password">
+                                                    @error('password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col">
+                                                        <div class="form-check">
+                                                            <input type="checkbox" class="form-check-input" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                                                            <label class="form-label" class="form-check-label" for="customControlInline">{{ __('Remember Me') }}</label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="d-grid mt-4">
+                                                    <button type="submit" class="btn btn-primary">
+                                                        {{ __('Login') }}
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end row -->
+            </div>
+        </div>
+        <!-- end Account pages -->
+
+        <!-- JAVASCRIPT -->
+        <script src="assets/libs/jquery/jquery.min.js"></script>
+        <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/libs/metismenu/metisMenu.min.js"></script>
+        <script src="assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="assets/libs/node-waves/waves.min.js"></script>
+
+        <script src="assets/js/app.js"></script>
+
+    </body>
+</html>
